47166dc37ed294ba2fd81aee03e14f6e01d06d9f
Will Holsten <willholsten@Wills-MacBook-Pro.local>
2016-03-29 18:01:21 +0100

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+jQuery(function($){
+    $( '.menu-btn' ).click(function(){
+    $(".responsive-menu").slideToggle();
+ })
+
+ $( '.response' ).click(function(){
+ $(".responsive-menu").slideToggle();
+  })
+})
